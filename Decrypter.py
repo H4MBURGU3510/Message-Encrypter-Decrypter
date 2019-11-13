0d52b5987263b307cb6f0fbe06b2f2cfdca27dd7
Marcos Agüero <wiredrat@gmail.com>
2019-11-13 17:21:26 +0100

    Accept abritrary length passwords

--- Decrypter.py
+++ Decrypter.py
@@ -1,13 +1,13 @@
-print ("What file do you want to decrypt?")
+print("What file do you want to decrypt?")
 result = ""
 name = input()
 print("What's the password?")
 password = input()
-f = open (name+".txt",'rb')
+plen = len(password)
+f = open(name + ".txt", 'rb')
 message = f.read()
-for byte in message:
-    result += chr( byte ^ ord(password))
-j = open (name+"(decrypted)"".txt",'w')
+for index, byte in enumerate(message):
+    result += chr(byte ^ ord(password[index % plen]))
+j = open(name + "(decrypted)"".txt", 'w')
 j.write(result)
 print("Decrypted file created successfully!")
-
--- Encrypter.py
+++ Encrypter.py
@@ -1,13 +1,14 @@
 
-print ("What will be the file's name?")
+print("What will be the file's name?")
 name = input()
 print("What do you want to write?")
 text = input()
 print("What will be the password?")
 password = input()
 result = bytearray()
-for letter in text:
-    result.append(ord(letter) ^ ord(password))
-f = open (name+'.txt','w')
+plen = len(password)  # Password lenght
+for index, letter in enumerate(text):
+    result.append(ord(letter) ^ ord(password[index % plen]))
+f = open(name + '.txt', 'w')
 f.write(result.decode())
 print("Encrypted file created successfully!")
